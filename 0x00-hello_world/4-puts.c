39a02b6f7c771afd5123235df8b43646598ac418
softraysjohn <softraystelecom@gmail.com>
2022-09-08 22:34:38 -0700

    c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+* main - entry point
+*
+* Return: main always return 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
