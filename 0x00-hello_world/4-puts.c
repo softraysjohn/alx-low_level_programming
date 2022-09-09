8dd487f26aa30226402a8497270763c6fa51322d
softraysjohn <softraystelecom@gmail.com>
2022-09-08 22:06:34 -0700

    c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(){
+/*
+ *function main
+ * program entry point
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
