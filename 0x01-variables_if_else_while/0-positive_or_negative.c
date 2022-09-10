bfbd784aaee41d6717700067b4b18f0fc971a543
softraysjohn <softraystelecom@gmail.com>
2022-09-09 17:33:33 -0700

    c-programming-2

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+* main - c-program entry point
+*
+* Return: always return 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >= 0)
+		printf("%s\n", " is positive");
+	if (n == 0)
+		printf("%s\n", " is zero");
+	if (n < 0)
+		printf("%s\n", " is negative");
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,7 @@
+# 0x01.c
+some statements in c-language:
+- if statement
+- for statement
+- while statement
+
+programs are betty compliant
