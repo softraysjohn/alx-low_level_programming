5e1f01d28c5b48e4a9a19436ef93448afb0f9d61
softraysjohn <softraystelecom@gmail.com>
2022-09-15 21:45:40 -0700

    c-programming-2

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_most_numbers - c-program entry point
+*
+* Return: description
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i < '9'; ++i)
+		_putchar(i);
+	_putchar('\n');
+}
+
