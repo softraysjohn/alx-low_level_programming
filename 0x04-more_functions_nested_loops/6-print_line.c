a7b6cf7d3a9f93144371f397a0ae31930b755460
softraysjohn <softraystelecom@gmail.com>
2022-09-16 05:33:28 -0700

    c-programming-2

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - line print
+* @n: length of line
+*
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+		_putchar('_');
+	_putchar('\n');
+}
+
