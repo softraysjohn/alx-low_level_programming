4e5bf342a87a2a19f908706c0950ac65eeb24fb7
softraysjohn <softraystelecom@gmail.com>
2022-09-16 06:01:36 -0700

    c-programming-2

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_diagonal - c-program entry point
+* @n: command-line count
+*
+* Return: description
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j <= i; ++j)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+}
+
