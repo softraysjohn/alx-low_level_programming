50281634622e63f7759bde3afa6f16b6f1402d9d
softraysjohn <softraystelecom@gmail.com>
2022-09-15 20:56:30 -0700

    c-programming-4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isdigit - assignment 1
+* @c: character
+*
+* Return: bool
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
