6d6b3e3a7a5da42c96a037649b4c62f503c084f9
softraysjohn <softraystelecom@gmail.com>
2022-09-15 22:18:13 -0700

    c-programming-2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - c-program entry point
+*
+* Return: description
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; ++i)
+		{
+			if (i > 9)
+				_putchar('1');
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
+	}
+}
+
