e969c4ee9b8c6d2af1bfa1b87801d6ec83fe5319
softraysjohn <softraystelecom@gmail.com>
2022-09-19 15:07:16 -0700

    c-programming-2

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* _strlen - like strlen
+* @s: string
+*
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while ('\0' != *(s++))
+		++len;
+	return (len);
+}
+
+/**
+* puts_half - print the second half of a string
+* @str: the string
+*
+*/
+void puts_half(char *str)
+{
+	int i, start, len;
+
+	len = _strlen(str);
+	start = len / 2 + len % 2;
+	i = 0;
+	while (*(str + start + i) != '\0')
+	{
+		_putchar(*(str + start + i));
+		i++;
+	}
+}
+
