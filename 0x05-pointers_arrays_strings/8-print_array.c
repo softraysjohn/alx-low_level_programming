248b60419bba665706edc5175c6df10dec64597f
softraysjohn <softraystelecom@gmail.com>
2022-09-19 17:13:24 -0700

    c-programming-2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		if (i != n - 1)
-			printf("%d, ", a[i]);
-		else
+		if (i == 0)
 			printf("%d", a[i]);
+		else
+			printf(",%c%d", 32, a[i]);
 	_putchar(10);
 }
 
