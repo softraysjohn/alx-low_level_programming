87e98f27ffaf810e37dc94da7d94057c70c5caef
softraysjohn <softraystelecom@gmail.com>
2022-09-21 20:15:05 -0700

    c-programming-6-More pointers,arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+/**
+* _strncpy - works like strncpy
+* @dest: destination
+* @src: sorce
+* @n: count
+*
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	return (dest);
+}
+
