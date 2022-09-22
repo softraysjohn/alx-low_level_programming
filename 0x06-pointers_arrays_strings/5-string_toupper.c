1f25782a58fbc411399a08deb9f8ccf8adf4371c
softraysjohn <softraystelecom@gmail.com>
2022-09-22 10:24:18 -0700

    c-programming-6-More pointers,arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,13 +2,14 @@
 * string_toupper - convert string to upper
 * @s: String to convert
 *
+* Return: the converted
 */
 char *string_toupper(char *s)
 {
 	int i, gap;
 
 	gap = 'a' - 'A';
-	i=0;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
