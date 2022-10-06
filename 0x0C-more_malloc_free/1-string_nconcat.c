db6a9528c9dff88152978c05a9b6502861fab022
softraysjohn <softraystelecom@gmail.com>
2022-10-06 09:19:59 -0700

    'Task-1.02'

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,55 +1,52 @@
-#include <stdlib.h>
-#include <limits.h>
-/**
-* s_len - finds the length of a string
-* @s: String
-*
-* Return: length
-*/
-unsigned int s_len(char *s)
-{
-	int i = 0;
-
-	if (s == NULL)
-		return (0);
-	while (i < UINT_MAX && s[i] != '\0')
-		++i;
-	return (i);
-}
+#include "main.h"
 
 /**
-* string_nconcat - concatenation with no allocation
-* @s1: First string
-* @s2: second string
-* @n: number of stuff from s2 (max)
-*
-* Return: pointer to destination
-*/
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char pointer
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len_1, len_2, i;
-	char *dst;
+	char *p;
+	unsigned int len_1 = 0, len_2 = 0, i;
 
-	len_1 = s_len(s1);
-	len_2 = s_len(s2);
-	dst =  malloc(sizeof(char) * (len_1 + len_2 + 1));
-	if (dst == NULL)
-		return (NULL);
-	/*end-if*/
-	i = 0;
-	while ((s1 != NULL) && s1[i] != '\0' && (i < len_1))
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_1] != '\0')
 	{
-		dst[i] = s1[i];
-		++i;
+		len_1++;
 	}
-	len_1 = i;
-	i = 0;
-	while ((s2 != NULL) && s2[i] != '\0' && i < n && (i < len_2))
+
+	while (s2[len_2] != '\0')
 	{
-		dst[i + len_1] = s2[i];
-		++i;
+		len_2++;
 	}
-	dst[i + len_1] = '\0';
-	return (dst);
+
+	if (n > len_2)
+	n = len_2;
+	p = malloc((len_1 + n + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (0);
+
+	for (i = 0; i < len_1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (; i < (len_1 + n); i++)
+	{
+		p[i] = s2[i - len_1];
+	}
+	p[i] = '\0';
+
+return (p);
 }
 
