d5f6f224a4406be70d5b31e56b04a504b50d935c
softraysjohn <softraystelecom@gmail.com>
2022-10-06 16:03:40 -0700

    'last_one'

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0); 
+		return (0);
 	}
 	dst = malloc(new_size);
 	if (dst == NULL)
