1b842b9830cf14c9b2a16ef2027965192f84a526
softraysjohn <softraystelecom@gmail.com>
2022-10-06 02:33:12 -0700

    Task-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - c-program entry point
+* @argc: command-line count
+* @argv: array of command-line strings
+*
+* Return: always 0
+*/
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* _putchar - display character
+* @c: character
+*
+*/
+void _putchar(char c)
+{
+	putchar(c);
+}
+
